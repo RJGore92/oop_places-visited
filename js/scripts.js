8a227d697f37ecdb28dea2fa689d1a65d070d029
Robert Gore <DarkburstGX@gmail.com>
2019-02-15 13:11:34 -0800

    Final commit for 4.1 practice of 'places you've been', contains finalized HTML and scripting with minor styling effects for buttons and bootstrap classes.

--- js/scripts.js
+++ js/scripts.js
@@ -10,8 +10,8 @@ PlacesList.prototype.addPlace = function (place) {
 };
 
 PlacesList.prototype.assignId = function () {
-  this.currentId += 1;
-  return this.currentId;
+  this.placeId += 1;
+  return this.placeId;
 };
 
 PlacesList.prototype.findPlace = function (id) {
@@ -80,6 +80,48 @@ Destination.prototype.fullLocation = function () {
   return locationFull;
 };
 
+// Manipulation functions for UI, Destinations, and PLaces List
+
+function removeFromPlaceLog(val) {
+  placesListLog.deletePlace(val);
+  console.log(val);
+  $("div#destination"+val).remove();
+}
+
+function showDetails(val) {
+  $("div#button"+val).slideToggle();
+  $("div#dest-body"+val).slideToggle();
+}
+
+function hideDetails(val) {
+  $("div#button"+val).slideToggle();
+  $("div#dest-body"+val).slideToggle();
+}
+
+function landmarksCheck(id) {
+  console.log(placesListLog.placesBeen[id-1]);
+  if (placesListLog.placesBeen[id-1].landmarks != "") {
+    var landmarksSplit = placesListLog.placesBeen[id-1].landmarks.split(", ");
+    console.log(landmarksSplit);
+    landmarksSplit.forEach(function(landmark) {
+      $("ul#landmarks"+id).append("<li>"+landmark+"</li>");
+    });
+  }
+  else {
+    $("ul#landmarks"+id).append("<li>No landmarks noted.</li>");
+  }
+}
+
+function notesCheck(id) {
+  if (placesListLog.placesBeen[id-1].notes != "") {
+    $("span#notes"+id).text(placesListLog.placesBeen[id-1].notes);
+  }
+  else {
+    $("span#notes"+id).text("No notes to mention.");
+  }
+}
+
+
 // User Interface Logic
 
 $(document).ready(function() {
@@ -93,19 +135,40 @@ $(document).ready(function() {
     var timeVisitedIn = $("input#time-of-year").val();
     var notesIn = $("input#notes-in").val();
     var testDestination = new Destination(destinationIn, locCityIn, locStateIn, locCountryIn, landmarksIn, timeVisitedIn, notesIn);
-    console.log(testDestination.fullLocation());
-    console.log(destinationIn);
-    console.log(locCityIn);
-    console.log(locStateIn);
-    console.log(locCountryIn);
-    console.log(landmarksIn);
-    console.log(timeVisitedIn);
-    console.log(notesIn);
-    console.log(testDestination);
+    placesListLog.addPlace(testDestination);
+    $("div#output-div").append(
+      "<div id='destination" + placesListLog.placeId + "' class='col-md-8'>" +
+        "<div class='card px-2 py-1'>" +
+          "<div class='card-heading bg-primary'><h3>" + testDestination.destinationName + "</h3><div id='button"+placesListLog.placeId+"'><button type='button' value='"+placesListLog.placeId+"' onClick='showDetails(this.value)'>Show Details</button></div><div><button type='button' class='red-button' value="+placesListLog.placeId+" onClick='removeFromPlaceLog(this.value)'>Delete this entry</button></div></div>" +
+          "<div class='card-body hidden-start bg-muted' id='dest-body"+placesListLog.placeId+"'>" +
+            "<div class='row'>" +
+              "<div class='col'>" +
+                "<h6>General location of destination:</h6>" +
+                "<p>" + testDestination.fullLocation() + "</p></div>" +
+              "<div class='col'>" +
+                "<h6>Landmarks near destination:</h6>" +
+                "<ul id='landmarks"+placesListLog.placeId+"'></ul></div>" +
+              "<div class='col'>" +
+                "<h6>Time of year visited:</h6>" +
+                "<p>" + testDestination.timeOfYear + "</p></div></div>" +
+            "<div class='row'>" +
+              "<div class='col'>" +
+                "<h6>Notes about location</h6>" +
+                "<p><span id='notes"+placesListLog.placeId+"'></span></p></div></div>" +
+            "<button type='button' value='"+placesListLog.placeId+"' onClick='hideDetails(this.value)'>Hide these details</button>" +
+            "</div></div></div>"
+    );
 
-    // currentAddressBook.addContact(contactToAdd);
-    // $("ul#contact-list").append(
-    //   "<li id='contact" + currentAddressBook.currentId + "'><button type='button' class='red-button' value='" + currentAddressBook.currentId + "' onClick='removeFromContacts(this.value)'>Delete this.</button> Name: " + contactToAdd.fullName() + ".  Phone Number: " + contactToAdd.phoneNumber + "</li>"
-    // );
+    landmarksCheck(placesListLog.placeId);
+    notesCheck(placesListLog.placeId);
+
+    // console.log(destinationIn);
+    // console.log(locCityIn);
+    // console.log(locStateIn);
+    // console.log(locCountryIn);
+    // console.log(landmarksIn);
+    // console.log(timeVisitedIn);
+    // console.log(notesIn);
+    // console.log(testDestination);
   });
 });
